2e396115963ec0c2267fe9c238c517ef8c16d13d
Weerachat Wongsawat <rabbit_roger@yahoo.com>
2026-04-30 12:17:34 +0700

    feat: add pom.xml and main application class for cancellation note PDF generation service

---
+++ src/main/java/com/wpanther/cancellationnote/pdf/CancellationNotePdfGenerationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.wpanther.cancellationnote.pdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CancellationNotePdfGenerationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CancellationNotePdfGenerationServiceApplication.class, args);
+    }
+}
